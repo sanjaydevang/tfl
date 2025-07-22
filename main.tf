498d93789c84d70578930422d14a53960571ee8b
Sanjay Vasanth Devang <sanjay_vasanth_devang@Sanjays-MacBook-Air.local>
2025-07-22 08:22:50 -0400

    initial

---
+++ main.tf
@@ -0,0 +1,28 @@
+# 1. Configure the AWS Provider
+# This tells Terraform we are going to be working with AWS
+# and in which region we want to create our resources.
+provider "aws" {
+  region = "us-east-1"
+}
+
+# 2. Define a Resource (our EC2 instance)
+# This block tells Terraform to create an AWS EC2 instance.
+resource "aws_instance" "web_server" {
+  # ami is the Amazon Machine Image ID - this one is for Amazon Linux 2
+  ami           = "ami-0c55b159cbfafe1f0" 
+  
+  # instance_type defines the size and power of the server
+  instance_type = "t2.micro"
+
+  # Tags are key-value pairs to help organize resources
+  tags = {
+    Name = "MyFirstTerraformServer"
+  }
+}
+
+# 3. Define an Output
+# This will print the public IP address of our server after it's created.
+output "web_server_public_ip" {
+  description = "The public IP address of our web server."
+  value       = aws_instance.web_server.public_ip
+}
